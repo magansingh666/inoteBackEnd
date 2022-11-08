2cb2c2e9e3d20b60273a335d271807976bb03a3c
Magan Singh <magansingh666@gmail.com>
2022-11-08 10:09:33 +0530

    added json web token and encrypted password with the help of bcrypt

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const connectToMongo = require('./db');
 connectToMongo();
 
-const express = require('express')
-const app = express()
-const port = 3002
+const express = require('express');
+const app = express();
+const port = 5000;
 
+
+app.use(express.json());
 
 //available routes
 app.use("/api/auth", require('./routes/auth.js'));
 //app.use("/api/notes", require('./routes/notes.js'));
 
 app.get('/', (req, res) => {
-  res.send('Hello Worl dfdfdf  d!')
+  res.send('Hello Worl dfdfdf  d!'); 
 })
 
 app.listen(port, () => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,54 @@
 const express = require("express");
-const router =  express.Router();
+const router = express.Router();
+const User = require("../models/User");
 
+const { body, validationResult } = require("express-validator");
+const { request } = require("express");
 
-router.get("/", (req, res)=> {
-    let obj = {"jjjj":"farzi objecti",  "mmmmm":"sdfdfdfdfdfdfd"};
-    res.json(obj);
-    
-})
+var bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+const JWT_SECRET = "Harryisagoodb$oy";
+
+//creates user by sending POST request.  No authentication required.
+router.post(
+  "/createuser",
+  // username must be an email
+  body("email", "enter a valid email").isEmail(),
+  // name and password must be at least 5 chars long
+  body("name", "enter a valid name").isLength({ min: 5 }),
+  body("password", "enter a valid password").isLength({ min: 5 }),
+
+  async (req, res) => {
+    // If there are validation errors inform user
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "user already exit" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+
+    user = await User.create({
+      name: req.body.name,
+      password: secPass,
+      email: req.body.email,
+    });
+
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+    const authtoken = jwt.sign(data, JWT_SECRET);
+
+    // res.json(user)
+    res.json({ authtoken });
+  }
+);
 
 module.exports = router;
