ad858c49a0e3e0e0bde7ecc92fc04a112348e037
Magan Singh <magansingh666@gmail.com>
2022-11-29 17:24:11 +0530

    set up maonge db atlas in place of local db

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const pass = process.env.DBPASS;
 const mongoUri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0";
+const mongoUri2 = `mongodb+srv://msingh111:${pass}@cluster0.myt71op.mongodb.net/?retryWrites=true&w=majority`;
 const connectToMongo = () => {
-mongoose.connect(mongoUri, ()=> {console.log("connected to mongo")})
+mongoose.connect(mongoUri2, ()=> {console.log("connected to mongo")})
 };
 module.exports = connectToMongo;
 
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const connectToMongo = require('./db');
 connectToMongo();
 
